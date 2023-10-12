48789a9ea50d336779288fe5ee77326faa2ed0f1
Sergey <ser1372@ukr.net>
2023-10-13 00:50:34 +0300

    up

--- resources/js/App/Products/ProductCard.vue
+++ resources/js/App/Products/ProductCard.vue
@@ -8,7 +8,7 @@ const state = defineProps({
 </script>
 
 <template>
-	<article  class="border-4 bg-[#FFFFFF] border-[#FFFFFF] overflow-hidden shadow-md m-auto rounded-[10px] w-full max-w-xl">
+	<article v-if="state.product.availability" class="border-4 bg-[#FFFFFF] border-[#FFFFFF] overflow-hidden shadow-md m-auto rounded-[10px] w-full max-w-xl">
 		<!-- Изображение товара -->
 		<img :src="'/storage/' + state.product.img" :alt="state.product.name" class="w-full object-cover transition hover:transform hover:scale-105 aspect-w-16 aspect-h-9">
 		<div class="p-4">
